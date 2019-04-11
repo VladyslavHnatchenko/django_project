c7c0d678e62a9a4bf4852d7db016b5570c49c16a
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-04-11 08:52:56 +0300

    4/11/2019

---
+++ trydjango19/crud.md
@@ -0,0 +1,10 @@
+--- GRUD ---
+create-read-update-delete
+
+we need permissions
+RETRIEVE(read) -- GET -- List/Search
+
+we don't need permissions
+CREATE -- POST -- Make New
+UPDATE(modify) -- PUT/PATCH -- Edit
+DELETE(destroy) -- DELETE -- Delete 
---
+++ trydjango19/posts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+)
+
+urlpatterns = [
+    path('', post_list, name='list'),
+    path('create/', post_create),
+    path('<int:id>/', post_detail, name='detail'),
+    path('<int:id>/edit/', post_update, name='update'),
+    path('<int:id>/delete/', post_delete),
+]
