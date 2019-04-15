db8e48da5394f0379d94d249c1cd26215e8eb3f1
vladyslav.hnatchenko <hnatchenko.vladyslav@gmail.com>
2019-04-15 09:41:42 +0300

    4/15/19

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,9 +1,12 @@
+from urllib.parse import quote_plus
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
+# import urllib
 
 
 def post_create(request):
@@ -22,10 +25,11 @@ def post_create(request):
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
-
+    share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
-        "instance": instance
+        "instance": instance,
+        "share_string": share_string,
     }
     return render(request, "post_detail.html", context)
 
